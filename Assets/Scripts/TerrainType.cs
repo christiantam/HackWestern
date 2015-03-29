fa8bceae5858495c84f71a9aa848e335a8892ee8
Christian Tam <ctam68@uwo.ca>
2015-03-28 21:56:06 -0400

    asdfasdf

---
+++ Assets/Scripts/TerrainType.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+abstract class Terrain : MonoBehaviour {
+
+	private int armor;
+	private string type;
+
+	public abstract string getType{
+		get;
+	}
+
+}
